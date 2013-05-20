9a97640fb83203e5a2d01f99d9606321328d1407
raccoons <raccoons@raccoons.com>
2013-05-20 02:04:31 +0100

    made more complicated again

--- testing.py
+++ testing.py
@@ -1,2 +1,11 @@
 # testing
-print 'hello world!'
+from sys import stdout
+from time import sleep
+
+word =''
+for i in [i for i in 'hello world!']:
+	word += i
+	stdout.write(word)
+	stdout.flush()
+	sleep(0.5)
+	stdout.write('\r')
